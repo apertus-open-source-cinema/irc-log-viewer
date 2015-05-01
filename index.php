41cbb55bada854d45e4320b4468998396d5abce6
Sebastian Pichelhofer <sebastian.pichelhofer@gmail.com>
2015-05-01 14:04:12 +0200

    removed piwik, not in use anymore

--- index.php
+++ index.php
@@ -178,21 +178,6 @@ if (!is_numeric($year)) {
 <meta http-equiv="refresh" content="300">
 </head>
 <body>
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(["trackPageView"]);
-  _paq.push(["enableLinkTracking"]);
-
-  (function() {
-    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.gerade.org/";
-    _paq.push(["setTrackerUrl", u+"piwik.php"]);
-    _paq.push(["setSiteId", "31"]);
-    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Piwik Code -->
 
 <?php
 $logdir = "LOG/".$year."-".$month."/";
